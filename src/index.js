51b4fba4e89d0f5978226301b30112f5626c7e63
diamond220qwert@mail.ru <diamond220qwert@mail.ru>
2023-01-17 09:52:50 +0300

    первый образ десктоп версии

---
+++ src/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import './styles/scss/main.scss';
+import './js/meetCaluclator.js';
+import './js/exampleSlider.js';
+import './js/ourTeamSlider.js';
+import './js/masterSlider.js';
+import './js/masterSlider.js';
+import './js/question.js';
